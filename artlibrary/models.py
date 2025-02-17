44f8883ad01a7344341f33d4d6c57aab1e231f82
afiap333 <115936869+afiap333@users.noreply.github.com>
2025-02-16 22:46:48 -0500

    added models

--- artlibrary/models.py
+++ artlibrary/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Patrons(models.Models):
+    patron_name=models.TextField(max_length=200)
+    patron_date_joined=models.DateTimeField("date joined")
+class Librarians(models.Models):
+    librarian_name=models.TextField(max_length=200)
+    librarian_date_joined=models.DateTimeField("date joined")
