ba7275da555204d7c5bf72fd7b21233c6544d743
pallavim8 <103335683+pallavim8@users.noreply.github.com>
2025-02-20 16:56:33 -0500

    default url routes to choice page

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,6 +20,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('artlibrary/',include("artlibrary.urls")),
-    path('accounts/', include('allauth.urls'))
-
+    path('accounts/', include('allauth.urls')),
+    path('',include("artlibrary.urls")),
 ]
