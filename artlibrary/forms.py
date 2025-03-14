605b631228975f7a70c4f811404819ab0b57b724
afiap333 <115936869+afiap333@users.noreply.github.com>
2025-03-13 23:26:52 -0400

    updated forms, added collection form

--- artlibrary/forms.py
+++ artlibrary/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import ArtSupply
+from .models import ArtSupply,Collection
 
 class AddArtSupplyForm(forms.ModelForm):
+    collection = forms.ModelChoiceField(
+        queryset=Collection.objects.all(), 
+        empty_label="Select a Collection", 
+        required=False
+    )
     class Meta:
         model = ArtSupply
         fields = ['name', 'image', 'quantity', 'pickup_location']
@@ -10,4 +15,15 @@ class AddArtSupplyForm(forms.ModelForm):
             'quantity': forms.NumberInput(attrs={'class': 'form-control'}),
             'pickup_location': forms.TextInput(attrs={'class': 'form-control'}),
             'image': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+            'description': forms.TextInput(attrs={'class': 'form-control'}),
         }
+class AddCollectionForm(forms.ModelForm):
+    class Meta:
+        model = Collection
+        fields = ['title','description','is_public']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.NumberInput(attrs={'class': 'form-control'}),
+            'is_public': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+        }
+
