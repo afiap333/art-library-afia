236a385b60d3587db0e24bef3da342e7b688dd7c
afiap333 <115936869+afiap333@users.noreply.github.com>
2025-04-20 20:52:26 -0400

    wrote new tests

--- artlibrary/tests.py
+++ artlibrary/tests.py
@@ -45,3 +45,24 @@ class createSupply(TestCase):
         self.supply=ArtSupply.objects.create(name="Test item",quantity=2,status="available",pickup_location="Charlottesville",item_type='single',added_by=self.user)
     def test_supply_created(self):
         self.assertEqual(self.supply.name,"Test item")
+class borrowSupply(TestCase):
+    def setUp(self):
+        self.client=Client()
+        self.user=User.objects.create_user(username="test",password="password", email="newpatron@gmail.com")
+        self.supply=ArtSupply.objects.create(name="Test item",quantity=2,status="available",pickup_location="Charlottesville",item_type='single',added_by=self.user)
+    def test_borrow(self):
+        self.supply.borrowed_by=self.user
+        self.assertEqual(self.supply.borrowed_by,self.user)
+class editSupply(TestCase):
+    #asked chatgpt about testing with forms
+    def setUp(self):
+        self.client=Client()
+        self.user=User.objects.create_user(username="test",password="password", email="newpatron@gmail.com")
+        self.supply=ArtSupply.objects.create(name="Test item",quantity=2,status="available",pickup_location="Charlottesville",item_type='single',added_by=self.user)
+    def test_edit(self):
+        url=reverse('edit_item',args=[self.supply.id])
+        updatedInfo={"name":"Edited item",}
+        response=self.client.post(url,updatedInfo)
+        self.supply.refresh_from_db()
+        self.assertEqual(response.status_code,302)
+        self.assertEqual(self.supply.name,"Edited item")
